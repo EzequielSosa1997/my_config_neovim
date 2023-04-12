674554d0a0b5098670215af446da4885f5ef6ebf
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-12 01:21:17 -0300

    add plugins of the cmp and cmp config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,72 +16,108 @@ vim.cmd([[
 ]])
 
 -- Use a protected call so we don't error out on first use
+--
+-- 
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+return
 end
 
 -- Have packer use a popup window
 packer.init({
-    display = {
-      open_fn = function()
-        return require('packer.util').float({ border = 'single' })
-      end
-    }
-  }
+display = {
+  open_fn = function()
+    return require('packer.util').float({ border = 'single' })
+  end
+}
+}
 )
 
 -- Install your plugins here
 return packer.startup(function(use)
 
-  --PACKER
-  use 'wbthomason/packer.nvim' 
-  use "nvim-lua/popup.nvim" 
-  use "nvim-lua/plenary.nvim" 
+--PACKER
+use 'wbthomason/packer.nvim' 
+use "nvim-lua/popup.nvim" 
+use "nvim-lua/plenary.nvim" 
 
-  --NEO-TREE
-  use {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v2.x",
-    requires = { 
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", 
-      "MunifTanjim/nui.nvim"
-    },
-    config = require 'plugins.config.neotree'
-  }
+--NEO-TREE
+use {
+"nvim-neo-tree/neo-tree.nvim",
+branch = "v2.x",
+requires = { 
+  "nvim-lua/plenary.nvim",
+  "nvim-tree/nvim-web-devicons", 
+  "MunifTanjim/nui.nvim"
+},
+config = require 'plugins.config.neotree'
+}
 
-  --BUFFER
-  use {
-    'akinsho/bufferline.nvim',
-    tag = "v3.*",
-    requires = 'nvim-tree/nvim-web-devicons',
-    config = require 'plugins.config.bufferline'
-  }
-  --LUALINE
-  use {
-    'nvim-lualine/lualine.nvim',
-    config = require 'plugins.config.lualine'
-  }
+--BUFFER
+use {
+'akinsho/bufferline.nvim',
+tag = "v3.*",
+requires = 'nvim-tree/nvim-web-devicons',
+config = require 'plugins.config.bufferline'
+}
+--LUALINE
+use {
+'nvim-lualine/lualine.nvim',
+config = require 'plugins.config.lualine'
+}
 
-  --TELESCOPE
-  use {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.1',
-  }
-  --THEME
-  use {
-    'folke/tokyonight.nvim',
-    config = require 'plugins.config.theme'
-  }
-  
-  --TOGGLETERM
-  use {
-    "akinsho/toggleterm.nvim",
-    tag = '*',
-    config = require 'plugins.config.toggleterm' 
-  }
+--TELESCOPE
+use {
+'nvim-telescope/telescope.nvim',
+tag = '0.1.1',
+}
+--THEME
+use {
+'folke/tokyonight.nvim',
+config = require 'plugins.config.theme'
+}
 
+--TOGGLETERM
+use {
+"akinsho/toggleterm.nvim",
+tag = '*',
+config = require 'plugins.config.toggleterm' 
+}
+
+--CMP------------------------------------------
+--
+
+use {
+  'neovim/nvim-lspconfig',
+  config = require 'plugins.lsp'
+}
+use {
+  'hrsh7th/cmp-nvim-lsp',
+  config = require 'plugins.config.cmp'  
+}
+use 'hrsh7th/cmp-buffer'
+use 'hrsh7th/cmp-path'
+use 'hrsh7th/cmp-cmdline'
+use 'hrsh7th/nvim-cmp'
+
+-- For vsnip users.
+use 'hrsh7th/cmp-vsnip'
+use 'hrsh7th/vim-vsnip'
+
+-- For luasnip users.
+use 'L3MON4D3/LuaSnip'
+use 'saadparwaiz1/cmp_luasnip'
+
+-- For ultisnips users.
+use 'SirVer/ultisnips'
+use 'quangnguyen30192/cmp-nvim-ultisnips'
+
+-- For snippy users.
+use 'dcampos/nvim-snippy'
+use 'dcampos/cmp-snippy'
+
+  --
+  -----------------------------------------------
 
   if PACKER_BOOTSTRAP then
     require('packer').sync()
