498380239e1010d24e0dbd9d0364f142b5726f8a
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-12 17:06:29 -0300

    update init packer LSP NULL-LS MASON CMP

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,7 +4,7 @@ local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
   PACKER_BOOTSTRAP = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  print("Installing packer close and reopen Neovim...")
+  print('Installing packer close and reopen Neovim...')
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -16,7 +16,7 @@ vim.cmd([[
 ]])
 
 -- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
+local status_ok, packer = pcall(require, 'packer')
 if not status_ok then
   return
 end
@@ -36,17 +36,17 @@ return packer.startup(function(use)
 
   --PACKER
   use 'wbthomason/packer.nvim' 
-  use "nvim-lua/popup.nvim" 
-  use "nvim-lua/plenary.nvim" 
+  use 'nvim-lua/popup.nvim' 
+  use 'nvim-lua/plenary.nvim' 
 
   --NEO-TREE
   use {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v2.x",
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v2.x',
     requires = { 
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", 
-      "MunifTanjim/nui.nvim"
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', 
+      'MunifTanjim/nui.nvim'
     },
     config = require 'plugins.config.neotree'
   }
@@ -54,7 +54,7 @@ return packer.startup(function(use)
   --BUFFER
   use {
     'akinsho/bufferline.nvim',
-    tag = "v3.*",
+    tag = 'v3.*',
     requires = 'nvim-tree/nvim-web-devicons',
     config = require 'plugins.config.bufferline'
   }
@@ -77,12 +77,40 @@ return packer.startup(function(use)
   
   --TOGGLETERM
   use {
-    "akinsho/toggleterm.nvim",
+    'akinsho/toggleterm.nvim',
     tag = '*',
     config = require 'plugins.config.toggleterm' 
   }
 
+  --LSP-MASON-NULL---------------------------------
+  --    
+  use 'neovim/nvim-lspconfig'
+	use	{
+    'williamboman/mason.nvim',
+    config = require 'plugins.lsp.mason'
+  }
+	use	'williamboman/mason-lspconfig.nvim'
+	use {
+    'jose-elias-alvarez/null-ls.nvim',
+    config = require 'plugins.lsp.null'
+  }-- Null ls is used for code formatting and pylint analysis
+  use {
+    'hrsh7th/nvim-cmp',
+    config = require 'plugins.lsp.cmp'  -- Autocompletion plugins
+  }
+  use 'L3MON4D3/LuaSnip'
+  use 'hrsh7th/cmp-nvim-lsp' -- Autocompletion with LSPs
+  --
+  --
+  --
+  --
+  --
+  --
+  --
+  --
+  ------------------------------------------------
 
+  
   if PACKER_BOOTSTRAP then
     require('packer').sync()
   end
