3db66981ced7e1c257069fa9738a3a0bd5cdbbfa
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-13 20:48:23 -0300

    change config for cmp

--- lua/plugins/lsp/cmp.lua
+++ lua/plugins/lsp/cmp.lua
@@ -1,18 +1,40 @@
-local ok, cmp = pcall( require, 'cmp')
-local okLua, luasnip = pcall(require,'luasnip')
-local keymap = require 'keymaps.lsp.lsp-cmp'
-if not okLua then return end
-if not ok then return end
+local ok, cmp = pcall(require, "cmp")
+local okLua, luasnip = pcall(require, "luasnip")
+local keymap = require("keymaps.lsp.lsp-cmp")
+if not okLua then
+	return
+end
+if not ok then
+	return
+end
 
-cmp.setup {
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end,
-    },
-    mapping = keymap(cmp,luasnip),
-    sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-    },
-}
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = keymap(cmp, luasnip),
+	sources = {
+		{ name = "path" },
+		{ name = "buffer", keyword_length = 3 },
+		{ name = "nvim_lsp" },
+		{ name = "luasnip", keyword_length = 2 },
+	},
+	window = {
+		documentation = cmp.config.window.bordered(),
+	},
+	formatting = {
+		fields = { "menu", "abbr", "kind" },
+		format = function(entry, item)
+			local menu_icon = {
+				nvim_lsp = "[LSP]",
+				luasnip = "[SNIP]",
+				buffer = "[BUF]",
+				path = "[PATH]",
+			}
+			item.menu = menu_icon[entry.source.name]
+			return item
+		end,
+	},
+})
