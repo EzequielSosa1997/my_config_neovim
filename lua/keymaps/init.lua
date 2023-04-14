f3c006ddd063072b748137270eade156a8d5cd0a
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-14 19:13:58 -0300

    <ADD> keymap saved in normal mode

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -1,17 +1,17 @@
 local set = vim.api.nvim_set_keymap
 local default_opts = { noremap = true, silent = true }
 local expr_opts = { noremap = true, expr = true, silent = true }
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
-require 'keymaps.config.neotree'
-require 'keymaps.config.bufferline'
-require 'keymaps.config.telescope'
-require 'keymaps.config.toggleterm'
+require("keymaps.config.neotree")
+require("keymaps.config.bufferline")
+require("keymaps.config.telescope")
+require("keymaps.config.toggleterm")
 
 --NAVEGATION
 set("", "<Left>", "<none>", default_opts)
-set("", "<Right>","<none>", default_opts)
-set("", "<Up>","<none>", default_opts)
+set("", "<Right>", "<none>", default_opts)
+set("", "<Up>", "<none>", default_opts)
 set("", "<Down>", "<none>", default_opts)
 
 set("i", "<C-h>", "<Left>", default_opts)
@@ -23,6 +23,4 @@ set("i", "<C-j>", "<Down>", default_opts)
 set("i", "<C-s>", "<cmd>w<cr>", default_opts)
 set("", "<C-x>", "<cmd>wq<cr>", default_opts)
 set("", "<A-q>", "<cmd>q<cr>", default_opts)
-
-
-
+set("n", "<leader>s", "<cmd>w<cr>", default_opts)
