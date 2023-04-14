ee1336ff060758cafcbed093babfb3871f011af8
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-14 20:15:01 -0300

    <ADD> config by GIT_GLUTTER

--- lua/setting/init.lua
+++ lua/setting/init.lua
@@ -43,3 +43,7 @@ vim.g.python3_host_prog = "/usr/bin/python3"
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.opt.clipboard = "unnamedplus"
+
+-- GITGUTTER
+vim.g.gitgutter_enable = 1
+vim.opt.updatetime = 100
