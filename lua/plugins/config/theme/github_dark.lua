96e31c32bde5330df2b752c691fcc54b94625e07
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-15 19:12:28 -0300

    <ADD> config theme github_dark

---
+++ lua/plugins/config/theme/github_dark.lua
@@ -0,0 +1,24 @@
+local _github, github = pcall(require, "github-theme")
+
+if not _github then
+	return
+end
+
+vim.cmd("colorscheme github_dark")
+
+github.setup({
+
+
+  -- Change the "hint" color to the "orange" color, and make the "error" color bright red
+  colors = { hint = 'orange', error = '#ff0000' },
+
+  -- Overwrite the highlight groups
+  overrides = function(c)
+    return {
+      htmlTag = { fg = c.red, bg = '#282c34', sp = c.hint, style = 'underline' },
+      DiagnosticHint = { link = 'LspDiagnosticsDefaultHint' },
+      -- this will remove the highlight groups
+      TSField = {},
+    }
+  end,
+})
