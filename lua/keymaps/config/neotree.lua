2d9c3e5e7aad5aaac7c3b6bd6a8d64cb56b23126
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-18 21:11:50 -0300

    <REMOVE><FUNCTION> keymaps in the wrong place

--- lua/keymaps/config/neotree.lua
+++ lua/keymaps/config/neotree.lua
@@ -1,7 +1,4 @@
 local set = vim.api.nvim_set_keymap
 local default_opts = { noremap = true, silent = true }
 local expr_opts = { noremap = true, expr = true, silent = true }
-
 set("n", "<leader>e", "<cmd>NeoTreeFloatToggle<cr>", default_opts)
-set("n", "<C-l>", "<c-w>l", default_opts)
-set("n", "<C-h>", "<c-w>h", default_opts)
