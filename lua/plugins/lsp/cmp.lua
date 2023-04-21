56993d3ebcfee0cb570473173bd1dcf24f4d9bfe
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-20 23:36:55 -0300

    <REFACTOR><ADD>new config cmp

--- lua/plugins/lsp/cmp.lua
+++ lua/plugins/lsp/cmp.lua
@@ -1,5 +1,6 @@
 local ok, cmp = pcall(require, "cmp")
 local okLua, luasnip = pcall(require, "luasnip")
+local okSni, snippy = pcall(require, "snippy")
 local keymap = require("keymaps.lsp.lsp-cmp")
 if not okLua then
 	return
@@ -8,33 +9,103 @@ if not ok then
 	return
 end
 
+if not okSni then
+	return
+end
+
+local kind_icons = {
+	Class = "ﴯ",
+	Color = "",
+	Constant = "",
+	Constructor = "",
+	Enum = "",
+	EnumMember = "",
+	Event = "",
+	Field = "",
+	File = "",
+	Folder = "",
+	Function = "",
+	Interface = "",
+	Keyword = "",
+	Method = "",
+	Module = "",
+	Operator = "",
+	Property = "ﰠ",
+	Reference = "",
+	Snippet = "",
+	Struct = "",
+	Text = "",
+	TypeParameter = "",
+	Unit = "",
+	Value = "",
+	Variable = "",
+}
+
 cmp.setup({
 	snippet = {
 		expand = function(args)
 			luasnip.lsp_expand(args.body)
 		end,
 	},
+	formatting = {
+		format = function(entry, vim_item)
+			vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind)
+			vim_item.menu = ({
+				nvim_lsp = "[LSP]",
+				spell = "[Spellings]",
+				zsh = "[Zsh]",
+				buffer = "[Buffer]",
+				ultisnips = "[Snip]",
+				treesitter = "[Treesitter]",
+				calc = "[Calculator]",
+				nvim_lua = "[Lua]",
+				path = "[Path]",
+				nvim_lsp_signature_help = "[Signature]",
+				cmdline = "[Vim Command]",
+			})[entry.source.name]
+			return vim_item
+		end,
+	},
 	mapping = keymap(cmp, luasnip),
-	sources = {
-		{ name = "path" },
-		{ name = "buffer", keyword_length = 3 },
-		{ name = "nvim_lsp" },
-		{ name = "luasnip", keyword_length = 2 },
+	completion = {
+		keyword_length = 1,
 	},
 	window = {
 		documentation = cmp.config.window.bordered(),
 	},
-	formatting = {
-		fields = { "menu", "abbr", "kind" },
-		format = function(entry, item)
-			local menu_icon = {
-				nvim_lsp = "[LSP]",
-				luasnip = "[SNIP]",
-				buffer = "[BUF]",
-				path = "[PATH]",
-			}
-			item.menu = menu_icon[entry.source.name]
-			return item
-		end,
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		-- For ultisnips users
+		{ name = "ultisnips" },
+		-- For vsnip users, uncomment the following.
+		{ name = "vsnip" },
+		-- For luasnip users, uncomment the following.
+		{ name = "luasnip" },
+		-- For snippy users, uncomment the following.
+		{ name = "snippy" },
+	}, {
+		{ name = "buffer" },
+	}, {
+		{ name = "nvim_lsp_signature_help" },
+	}, {
+		{ name = "path" },
+	}),
+})
+
+cmp.setup.cmdline(":", {
+	sources = cmp.config.sources({
+		{ name = "path" },
+		{ name = "cmdline" },
+	}),
+})
+
+cmp.setup.cmdline("/", {
+	sources = {
+		{ name = "buffer" },
 	},
 })
+
+vim.cmd([[
+let g:UltiSnipsJumpForwardTrigger='<Tab>'
+let g:UltiSnipsJumpBackwardTrigger='<S-Tab>'
+]])
