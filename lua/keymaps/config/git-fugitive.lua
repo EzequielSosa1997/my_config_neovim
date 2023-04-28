616e479e395f90ba215c13ce7a58c236aa39aed6
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-28 13:05:23 -0300

    <ADD> new keymaps pull and push git

--- lua/keymaps/config/git-fugitive.lua
+++ lua/keymaps/config/git-fugitive.lua
@@ -2,3 +2,5 @@ vim.g.mapleader = " "
 local set = vim.api.nvim_set_keymap
 local default_opts = { noremap = true, silent = true }
 set("n", "<leader>g", "<cmd>G<cr>", default_opts)
+set("n", "<leader>gp", "<cmd>G push<cr>", default_opts)
+set("n", "<leader>gl", "<cmd>G pull<cr>", default_opts)
