f0a072193cc58ec7b50051de2a5bab526711e0a2
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-05-01 17:10:05 -0300

    <ADD> implements which-key to toggleterm

--- lua/keymaps/config/toggleterm.lua
+++ lua/keymaps/config/toggleterm.lua
@@ -2,4 +2,19 @@ local set = vim.api.nvim_set_keymap
 local default_opts = { noremap = true, silent = true }
 local expr_opts = { noremap = true, expr = true, silent = true }
 
-vim.keymap.set('', '<leader>t','<cmd>ToggleTerm<cr>', default_opts)
+local okWich, with_key = pcall(require, "which-key")
+local addCommand = require("keymaps.config.whitch-key")
+
+if not okWich then
+	return
+end
+
+with_key.register({
+	t = {
+		name = "Terminal",
+		n = { addCommand("lua _NODE_TOGGLE()"), "Node" },
+		f = { addCommand("ToggleTerm direction=float"), "Float" },
+		t = { addCommand("ToggleTerm size=10 direction=horizontal"), "Horizontal" },
+		v = { addCommand("ToggleTerm size=80 direction=vertical"), "Vertical" },
+	},
+}, { prefix = "<leader>" })
