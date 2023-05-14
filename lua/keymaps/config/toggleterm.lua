b4b8475b39870c41e3f2126cac2c4d11cf43e622
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-05-14 18:30:13 -0300

    <ADD> new keymaps Trans ES -> EN in toggleterm

--- lua/keymaps/config/toggleterm.lua
+++ lua/keymaps/config/toggleterm.lua
@@ -1,7 +1,3 @@
-local set = vim.api.nvim_set_keymap
-local default_opts = { noremap = true, silent = true }
-local expr_opts = { noremap = true, expr = true, silent = true }
-
 local okWich, with_key = pcall(require, "which-key")
 local addCommand = require("keymaps.config.whitch-key")
 
@@ -13,6 +9,8 @@ with_key.register({
 	t = {
 		name = "Terminal",
 		n = { addCommand("lua _NODE_TOGGLE()"), "Node" },
+		s = { addCommand("lua _TRANSESEN_TOGGLE()"), "Trans ES -> EN" },
+		e = { addCommand("lua _TRANSENES_TOGGLE()"), "Trans EN -> ES" },
 		f = { addCommand("ToggleTerm direction=float"), "Float" },
 		t = { addCommand("ToggleTerm size=10 direction=horizontal"), "Horizontal" },
 		v = { addCommand("ToggleTerm size=80 direction=vertical"), "Vertical" },
