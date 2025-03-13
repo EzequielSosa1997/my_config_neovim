d38f0f0dcf7b1a50fb4d9bdc9a3f0de634b5508a
EzequielSosa1997 <ezequiel.ignacio.sosa@gmail.com>
2025-03-12 23:47:58 -0300

    fix: delete format prisma

--- lua/plugins/formant.lua
+++ lua/plugins/formant.lua
@@ -8,22 +8,8 @@ return {
         python = { "isort", "black" },
         javascript = { "prettierd", "prettier", stop_after_first = true },
         typescript = { "prettier" },
-        prisma = { "prisma" },
         sh = { "shfmt" },
       },
-      formatters = {
-        prisma = {
-          command = "prisma",
-          args = { "format" },
-          stdin = false, -- Prisma no soporta entrada estándar para formatear
-        },
-      },
-    })
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = "*",
-      callback = function(args)
-        require("conform").format({ bufnr = args.buf })
-      end,
     })
   end,
 }
